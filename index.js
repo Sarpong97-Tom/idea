4d6ed12d38472971a1c6d1adb8a4108c0e78842d
tom <sarpongthomas05@gmail.com>
2020-09-19 14:05:02 +0000

    added starting log

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require('express'); 
+const express = require("express");
 const app = express();
-app.use(express.static('public'));
-app.get('/', (req, res) => {
-     res.sendFile('index.html', {root: __dirname + '/public/'});
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname + "/public/" });
 });
-app.get('/about', (req, res) => {
-    res.sendFile('about.html', {root: __dirname + '/public/'});
+app.get("/about", (req, res) => {
+  res.sendFile("about.html", { root: __dirname + "/public/" });
 });
-app.listen(process.env.PORT || 8000)
+PORT = 8000;
+app.listen(process.env.PORT || PORT, () => {
+  console.log("App is working on " + PORT);
+});
